a1242d17a3d4b4831204cd7c7ff9485211d6b69d
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-24 22:23:49 +0200

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,8 +2,6 @@ import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
 import { useAuth } from 'hooks/useAuth';
 import { UserName, Wrapper, Button } from './UserMenu.styled';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
@@ -17,11 +15,7 @@ const UserMenu = () => {
     <Wrapper>
       <UserName>Welcome, {user.name}</UserName>
       <Button type="button" onClick={onLogOut}>
-        {' '}
-        <FontAwesomeIcon
-          icon={faRightFromBracket}
-          style={{ color: '#a3c6c4' }}
-        />{' '}
+        LogOut
       </Button>
     </Wrapper>
   );
